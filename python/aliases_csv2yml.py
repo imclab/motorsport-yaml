c434a8dfd90be43651c56a1b2a622b9835e9f28f
Kevin J. Lin <kevin@seatgeek.com>
2013-03-31 14:42:29 -0700

    Fixed NHRA postseason YAML

--- python/aliases_csv2yml.py
+++ python/aliases_csv2yml.py
@@ -1,12 +1,13 @@
 import csv
-with open('../csv/nhra-post-season.csv', 'rb') as csv_input:
+with open('../csv/nhra-regular-season.csv', 'rb') as csv_input:
     csv_array = csv.reader(csv_input)
     next(csv_array)
     for row in csv_array:
         print '-' + '\n' + '\t' + 'name: ' + row[0]
         print '\t' 'series: ' + row[1]
         print '\t' 'track: ' + row[2]
-        print '\t' 'aliases:'
+        # print '\t' 'postseason: true'
+        print '\t' 'aliases:'        
         aliases = row[3].split('\n')
         for row in aliases:
           print '\t\t' + row
